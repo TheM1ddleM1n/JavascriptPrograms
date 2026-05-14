12ea9dcd632546bcf42dcbeeefbdda180051288b
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-14 15:20:17 +0100

    Create binarySearch.js

---
+++ Algorithms/binarySearch.js
@@ -0,0 +1,13 @@
+function binarySearch(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] < target) low = mid + 1;
+    else high = mid - 1;
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 3, 5, 7, 9, 11], 7));

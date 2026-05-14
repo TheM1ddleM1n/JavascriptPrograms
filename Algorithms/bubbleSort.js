1de19631c8fc931a8082beb83757c8bbec5d3706
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-05-14 15:19:53 +0100

    Create bubbleSort.js

---
+++ Algorithms/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+  const a = [...arr];
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        [a[j], a[j + 1]] = [a[j + 1], a[j]];
+      }
+    }
+  }
+  return a;
+}
+
+console.log(bubbleSort([5, 3, 8, 1, 2]));
